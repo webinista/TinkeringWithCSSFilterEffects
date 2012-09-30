9aa7d6118d2ef45873286996598adcb86790caae
Tiffany B. Brown <tiffany@webinista.com>
2012-09-30 16:35:16 -0700

    Adding reset functionality.

--- filters.js
+++ filters.js
@@ -1,22 +1,45 @@
+/*
+Mimics Array.prototype.map().
+Need to use arguments[0] instead of this
+inside of func. Rethink this.
+*/
+NodeList.prototype.mappish = function(func){
+    var nodelistarr = [], len = this.length, i;
+    for(i=0; i<len; i++){
+        nodelistarr[i] = this[i];
+    }
+    nodelistarr.map(func);
+}
+
 var form = document.getElementsByTagName('form')[0];
+
 var updatevalue = function(e){
     var val, value, valuewrap;
 
-    e.target.id == 'blur' ? val = e.target.value + 'px' : val = e.target.value;
+    if( e.target.type === 'range' ){
 
-    value = document.createTextNode(val),
-    valuewrap = e.target.nextElementSibling;
+        e.target.id == 'blur' ? val = e.target.value + 'px' : val = e.target.value;
 
+        value = document.createTextNode(val),
+        valuewrap = e.target.nextElementSibling;
 
-
-    valuewrap.replaceChild(value,valuewrap.firstChild);
+        valuewrap.replaceChild(value,valuewrap.firstChild);
+    } else {
+        console.log('not a range element.');
+    }
 }
 
-var coo = function(e){
-    var filter = document.getElementById('filter');
-    console.log(filter);
+var resethandler = function( ){
+    var filter = document.getElementById('filter'),
+        values = document.getElementsByClassName('value');
+    values.mappish( function(){
+        var it = arguments[0];
+        it.replaceChild( document.createTextNode( it.previousElementSibling.getAttribute('min') ), it.firstChild );
+    });
+    filter.style.webkitFilter = '';
+    filter.removeAttribute('style');
 }
 
 form.addEventListener('change',updatevalue,false);
-form.addEventListener('change',coo,false);
+form.addEventListener('reset',resethandler,false);
 
