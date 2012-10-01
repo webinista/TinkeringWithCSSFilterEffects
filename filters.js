d73afd2269ff8c532d46ba7d533614ae41719fcd
Tiffany B. Brown <tiffany@webinista.com>
2012-09-30 17:29:03 -0700

    Tweaking the start values so they make sense when we adjust them.

--- filters.js
+++ filters.js
@@ -16,22 +16,28 @@ var form = document.getElementsByTagName('form')[0];
 var updatevalue = function(e){
     var val, value, valuewrap, style = '',
         filter = document.getElementById('filter')
-        filter.style.webkitFilter = 'grayscale(0) sepia(0) saturate(0) hue-rotate(0) invert(0) opacity(1) brightness(0) contrast(0) blur(0) drop-shadow(0px 0px 0px #000)';
+        filter.style.webkitFilter = 'grayscale(0) sepia(0) saturate(0) hue-rotate(0) invert(0) opacity(1) brightness(0) contrast(1) blur(0) drop-shadow(0px 0px 0px #000)';
 
     if( e.target.type === 'range' ){
 
-        e.target.id == 'blur' ? val = e.target.value + 'px' : val = e.target.value;
+        if( e.target.id == 'blur' ){
+            val = e.target.value + 'px';
+        } else if(e.target.id == 'hue-rotate') {
+            val = e.target.value + 'deg';
+        } else {
+            val = e.target.value;
+        }
 
         value = document.createTextNode(val),
         valuewrap = e.target.nextElementSibling;
 
-        style = e.target.id+'('+e.target.value+')';
-
+        style = e.target.id+'('+val+')';
 
         valuewrap.replaceChild(value,valuewrap.firstChild);
     } else {
         console.log('not a range element.');
     }
+    console.log(style);
 
     filter.style.webkitFilter = style;
 
@@ -42,7 +48,7 @@ var resethandler = function( ){
         values = document.getElementsByClassName('value');
     values.mappish( function(){
         var it = arguments[0];
-        it.replaceChild( document.createTextNode( it.previousElementSibling.getAttribute('min') ), it.firstChild );
+        it.replaceChild( document.createTextNode( it.previousElementSibling.getAttribute('value') ), it.firstChild );
     });
     filter.style.webkitFilter = '';
     filter.removeAttribute('style');
