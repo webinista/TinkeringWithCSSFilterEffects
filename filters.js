0710342a81e33aa853717fc4e14b952b12c9f9e5
Tiffany B. Brown <tiffany@webinista.com>
2012-10-01 14:32:04 -0700

    Jiggling the UI a touch. Rewiring things so that we add a webkit-filter style on page load.

--- filters.js
+++ filters.js
@@ -11,49 +11,82 @@ NodeList.prototype.mappish = function(func){
     nodelistarr.map(func);
 }
 
-var form = document.getElementsByTagName('form')[0];
+var form,
+    resethandler,
+    obj = document.getElementById('filtered'),
+    startstyle,
+    update;
 
-var updatevalue = function(e){
-    var val, value, valuewrap, style = '',
-        filter = document.getElementById('filter')
-        filter.style.webkitFilter = 'grayscale(0) sepia(0) saturate(0) hue-rotate(0) invert(0) opacity(1) brightness(0) contrast(1) blur(0) drop-shadow(0px 0px 0px #000)';
+update = function(e){
+    var f = obj.style.webkitFilter, newfilt,
+        val = e.target.nextElementSibling,
+        dropshad;
 
-    if( e.target.type === 'range' ){
+        console.log( f );
 
-        if( e.target.id == 'blur' ){
-            val = e.target.value + 'px';
-        } else if(e.target.id == 'hue-rotate') {
-            val = e.target.value + 'deg';
-        } else {
-            val = e.target.value;
-        }
-
-        value = document.createTextNode(val),
-        valuewrap = e.target.nextElementSibling;
-
-        style = e.target.id+'('+val+')';
-
-        valuewrap.replaceChild(value,valuewrap.firstChild);
-    } else {
-        console.log('not a range element.');
+    switch( e.target.id ){
+        case 'blur':
+            newfilt = f.replace(/blur\([0-9]+/g,'blur('+(e.target.value*1) );
+            val.replaceChild(document.createTextNode(e.target.value),val.firstChild);
+            break;
+         case 'brightness':
+            newfilt = f.replace(/brightness\([\.0-9]+/,'brightness('+(e.target.value*1) );
+            val.replaceChild(document.createTextNode(e.target.value),val.firstChild);
+            break;
+         case 'contrast':
+            newfilt = f.replace(/contrast\([0-9]+/,'contrast('+(e.target.value*1) );
+            val.replaceChild(document.createTextNode(e.target.value),val.firstChild);
+            break;
+         case 'grayscale':
+            newfilt = f.replace(/grayscale\([\.0-9]+/,'grayscale('+(e.target.value*1) );
+            val.replaceChild(document.createTextNode(e.target.value),val.firstChild);
+            break;
+         case 'invert':
+            newfilt = f.replace(/invert\([\.0-9]+/,'invert('+(e.target.value*1) );
+            val.replaceChild(document.createTextNode(e.target.value),val.firstChild);
+            break;
+         case 'hue-rotate':
+            newfilt = f.replace(/hue\-rotate\([0-9]+/,'hue-rotate('+e.target.value );
+            val.replaceChild(document.createTextNode(e.target.value),val.firstChild);
+            break;
+         case 'opacity':
+            newfilt = f.replace(/opacity\([0-9]+/,'opacity('+e.target.value );
+            val.replaceChild(document.createTextNode(e.target.value),val.firstChild);
+            break;
+         case 'sepia':
+            console.log(e.target.value);
+            newfilt = f.replace(/sepia\([\.0-9]+/,'sepia('+(e.target.value*1) );
+            val.replaceChild(document.createTextNode(e.target.value),val.firstChild);
+            break;
+         case 'saturate':
+            newfilt = f.replace(/saturate\([0-9]+/,'saturate('+e.target.value );
+            val.replaceChild(document.createTextNode(e.target.value),val.firstChild);
+            break;
+         case 'drop-shadow-x':
+         case 'drop-shadow-y':
+         case 'drop-shadow-blur':
+         case 'drop-shadow-color':
+            dropshad = document.querySelectorAll('input[name|=drop-shadow]')
+            newfilt = f.replace(/(drop-shadow\()rgb\([0-9]+, [0-9]+, [0-9]+\) [0-9]+px [0-9]+px [0-9]+px\)/,"$1"+dropshad[0].value+'px '+dropshad[1].value+'px '+dropshad[2].value+'px '+dropshad[3].value+')');
+            break;
     }
-    console.log(style);
 
-    filter.style.webkitFilter = style;
-
+    obj.style.webkitFilter = newfilt;
 }
 
-var resethandler = function( ){
-    var filter = document.getElementById('filter'),
-        values = document.getElementsByClassName('value');
+
+form = document.getElementsByTagName('form')[0];
+resethandler = function(){
+    var values = document.getElementsByClassName('value');
     values.mappish( function(){
         var it = arguments[0];
         it.replaceChild( document.createTextNode( it.previousElementSibling.getAttribute('value') ), it.firstChild );
     });
-    filter.style.webkitFilter = '';
-    filter.removeAttribute('style');
+    obj.style.webkitFilter = startstyle;
 }
-
-form.addEventListener('change',updatevalue,false);
+form.addEventListener('input',update,false);
 form.addEventListener('reset',resethandler,false);
-
+window.addEventListener('load',function(e){
+    obj.style.webkitFilter = "blur(0px) brightness(0) contrast(100%) drop-shadow(0px 0px 0px #000) grayscale(0) hue-rotate(0deg) invert(0) opacity(1) saturate(1) sepia(0)";
+    console.log( obj.style.webkitFilter );
+},false);
